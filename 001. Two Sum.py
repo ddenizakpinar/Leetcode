0596ec407b98130866347c8da1cafec1122eb1ec
Deniz Akpınar <ddenizakpinar@gmail.com>
2020-01-16 17:55:14 +0300

    001. Two Sum

---
+++ 001. Two Sum.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/two-sum/
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
+
+
+nums = [2, 5, 4, 11]
+target = 9
+s = Solution()
+print(s.twoSum(nums, target))
